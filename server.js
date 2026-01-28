d5ec3b6e54bd89fb583bce4eacc0dd4d8be28749
Frana Jaya <franajaya@Franas-MacBook-Pro.local>
2026-01-28 11:49:02 +0800

    bind 0.0.0.0 for railway

--- server.js
+++ server.js
@@ -108,4 +108,4 @@ app.post("/process", async (req, res) => {
 /* ================= SERVER START ================= */
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server jalan di port " + PORT));
+app.listen(PORT, "0.0.0.0", () => console.log("Server jalan di port " + PORT));
